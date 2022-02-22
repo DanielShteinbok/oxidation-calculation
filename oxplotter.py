2e454f4d9f9e2f72e8a88a95ba06f7ce88506a8c
Daniel Shteinbok <dshteinbok@gmail.com>
2022-02-22 17:28:22 -0500

    fixed uits issue in oxplotter

--- oxplotter.py
+++ oxplotter.py
@@ -11,13 +11,16 @@ import sympy
 def plotMassGainNumpy(code, substitutions={}):
     times = numpy.arange(0, 8*60*60, 1000)
     lamfunc, units_expression = values.getMassGainNumpyLambda(code, substitutions)
-    fig, axis = plt.subplots()
-    axis.plot(times, lamfunc(times))
+    fig, axes = plt.subplots()
+    axes.plot(times, lamfunc(times))
     conditions = values.getExperimentConds(code, substitutions)
     plt.title("Pressure: " + str(conditions[equations.P_O2]/pascal) + ", Temperature: " + str(conditions[equations.T]/kelvin))
     #plt.show()
-    #TODO return the figure and the units
-    return fig, sympy.simplify(units_expression.subs(equations.t,second))
+    final_units = sympy.simplify(units_expression.subs(equations.t,second))
+    axes.set_ylabel("Thickness, " + str(final_units))
+    axes.set_xlabel("Time, seconds")
+    #return fig, sympy.simplify(units_expression.subs(equations.t,second))
+    return fig, final_units
 
 def plotThicknessNumpy(code, start=0, stop=28800, step=1000, substitutions={}):
     times = numpy.arange(start, stop, step)
@@ -25,11 +28,14 @@ def plotThicknessNumpy(code, start=0, stop=28800, step=1000, substitutions={}):
     fig, axes = plt.subplots()
     axes.plot(times, lamfunc(times))
     conditions = values.getExperimentConds(code, substitutions)
+
     plt.title("Pressure: " + str(conditions[equations.P_O2]/pascal) + ", Temperature: " + str(conditions[equations.T]/kelvin))
-    final_units = sympy.simplify(units_expression.subs([(equations.t,second), (equations.e, 1), (equations.F_p, 1)])).as_coeff_Mul()[1]/(-1 + sympy.sqrt(5))
+    #final_units = sympy.simplify(units_expression.subs([(equations.t,second), (equations.e, 1), (equations.F_p, 1)])).as_coeff_Mul()[1]/(-1 + sympy.sqrt(5))
+    final_units = sympy.simplify(units_expression.subs(equations.t,second))
     axes.set_ylabel("Thickness, " + str(final_units))
     axes.set_xlabel("Time, seconds")
     #plt.show()
     #TODO return the figure and the units
-    return fig, sympy.simplify(units_expression.subs(equations.t,second))
+    #return fig, sympy.simplify(units_expression.subs(equations.t,second))
+    return fig, final_units
     #return fig, sympy.simplify(units_expression.subs([(equations.t,second), (equations.e, 1), (equations.F_p, 1)])).as_coeff_Mul()[1]/(-1 + sympy.sqrt(5))
